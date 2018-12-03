3ca1167616f7e2a8492ab18377cc65d75a2cd7f7
Siva <sivajrm@gmail.com>
2018-12-03 00:16:14 -0800

    With category_id foreigh reference and grouped category_id

--- PredictSales.py
+++ PredictSales.py
@@ -13,6 +13,16 @@ excludeColumns =['date_block_num', 'date']
 
 df.drop(excludeColumns, axis=1, inplace=True)
 
+i = 0
+def appendCategoryIdToDF(eachDFRow):
+    item_category_id = items[items['item_id'] == eachDFRow['item_id']].iloc[0]['item_category_id']
+    #print("before",item_category_id)
+    groupedCategoryID = item_categories[item_categories['item_category_id'] == item_category_id].iloc[0]['category_id']
+    #print("after",groupedCategoryID)
+    eachDFRow['category_id'] = groupedCategoryID
+    print(df[:5])
+    return eachDFRow
+
 zeroColumns = ['item_cnt_day','item_price']
 
 #Replace negative values in columns to zero
@@ -22,3 +32,18 @@ for col in zeroColumns:
     df[df[col] < 0] = 0
 print('After:')
 print(sum(n < 0 for n in df['item_cnt_day']))
+
+# =============================================================================
+# read sales_train dataframe df and update the category_id in sales_train 
+# dataframe df as per the new grouped category_id in item_categories.csv 
+# =============================================================================
+item_categories = pd.read_csv("./data/item_categories.csv")  
+items = pd.read_csv("./data/items.csv") 
+# =============================================================================
+# Call appendCategoryIdToDF() to generate the category id for additional inference
+# This method will create a category id through Category grouping process
+# =============================================================================
+df = df.apply(appendCategoryIdToDF,axis=1)
+ 
+
+
